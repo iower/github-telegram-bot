814877855a2b4c24a2587da6140f00728df3365f
iower <iower@tutanota.com>
2023-11-12 18:46:29 +0500

    Date log

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const IS_DEBUG = true;
 
-console.log('Launch github-telegram-bot...');
+console.log((new Date()).toISOString(), 'Launch github-telegram-bot...');
 
 const fs = require('fs');
 const fetch = require('node-fetch');
@@ -10,15 +10,15 @@ const config = require('./config.json');
 const bot = new TelegramBot(config.TELEGRAM_BOT_TOKEN, {polling: true});
 
 bot.on('message', (msg) => {
-  console.log('message', msg);
+  console.log((new Date()).toISOString(), 'message', msg);
   const chatId = msg.chat.id;
   if (chatId == config.TELEGRAM_OWNER_ID) {
     bot.sendMessage(chatId, 'Up!');
   }
 });
 
-// bot.sendMessage(config.TELEGRAM_OWNER_ID, 'Launched!');
-console.log('Launched!');
+bot.sendMessage(config.TELEGRAM_OWNER_ID, 'Launched!');
+console.log((new Date()).toISOString(), 'Launched!');
 
 const savedCommitsFileName = `saved-commits-${config.GITHUB_REPO_OWNER_NAME}-${config.GITHUB_REPO_NAME}`;
 const savedCommitsFilePath = `./${savedCommitsFileName}.json`;
@@ -30,7 +30,7 @@ const process = async () => {
     const fetchedCommits = await response.json();
 
     if (fetchedCommits?.message?.includes('limit exceeded')) {
-      console.error(fetchedCommits.message);
+      console.error((new Date()).toISOString(), fetchedCommits.message);
       return;
     }
 
@@ -39,7 +39,7 @@ const process = async () => {
     try {
       savedCommits = JSON.parse(fs.readFileSync(savedCommitsFilePath, 'utf8'))
     } catch (e) {
-      console.info(`Will be created: ${savedCommitsFilePath}`);
+      console.info((new Date()).toISOString(), `Will be created: ${savedCommitsFilePath}`);
       saveCommits(fetchedCommits)
       return;
     }
@@ -55,7 +55,7 @@ const process = async () => {
     };
 
     if (newCommits.length) {
-      console.log(`New commits! (${newCommits.length})`);
+      console.log((new Date()).toISOString(), `New commits! (${newCommits.length})`);
       for (let i = 0; i < newCommits.length; i++) {
         const newCommit = newCommits[i];
         await sendCommitSummary(newCommit);
@@ -63,7 +63,7 @@ const process = async () => {
       saveCommits(fetchedCommits);
     }
   } catch (err) {
-    console.error('process err |', err);
+    console.error((new Date()).toISOString(), 'process err |', err);
   }
 }
 
@@ -73,10 +73,10 @@ setInterval(() => {
 }, config.UPDATE_INTERVAL_SEC * 1000);
 
 const saveCommits = (commits) => {
-  console.log(`Save commits to ${savedCommitsFilePath}`);
+  console.log((new Date()).toISOString(), `Save commits to ${savedCommitsFilePath}`);
   fs.writeFileSync(savedCommitsFilePath, JSON.stringify(commits), (e) => {
     if (e) {
-      console.error(`Cannot write ${savedCommitsFilePath}`);
+      console.error((new Date()).toISOString(), `Cannot write ${savedCommitsFilePath}`);
       throw new Error(e)
     }
   });
