00551eca9362b4a9eb27edd1f026880615c4cdea
iower <iower@tutanota.com>
2023-11-12 16:59:03 +0500

    Improve message

--- index.js
+++ index.js
@@ -21,22 +21,31 @@ const send = async () => {
   const url = `https://api.github.com/repos/${config.GITHUB_REPO_OWNER_NAME}/${config.GITHUB_REPO_NAME}/commits`
   const response = await fetch(url);
   const commits = await response.json();
-  const commit = commits[0];
+  const commit = commits[1];
 
-  const message = `👤${commit?.author?.login || commit?.commit?.author?.name || comimt?.commit?.committer?.name || '?'}\n📝${commit?.commit?.message}\n🔗[${commit?.sha?.slice(0, 7)}]`
+  const message = [
+    `👤${commit?.author?.login || commit?.commit?.author?.name || comimt?.commit?.committer?.name || '?'}`,
+    `📝${commit?.commit?.message || '?'}`,
+    `🔗<a href='${commit?.html_url || '?'}'>${commit?.sha?.slice(0, 7) || '?'}</a>`
+  ].join('\n')
 
   console.log(commit)
   console.log(message)
 
-  bot.sendMessage(config.TELEGRAM_OWNER_ID, message);
+  bot.sendMessage(config.TELEGRAM_OWNER_ID, message, {
+    parse_mode: 'HTML',
+    disable_web_page_preview: true
+  });
 
   /*
   bot.sendMessage(
     config.TELEGRAM_CHAT_ID,
     message,
-    config.TELEGRAM_TOPIC_ID
-      ? { reply_to_message_id: config.TELEGRAM_TOPIC_ID }
-      : undefined
+    {
+      reply_to_message_id: config.TELEGRAM_TOPIC_ID || undefined,
+      parse_mode : 'HTML',
+      disable_web_page_preview: true
+    }
   );
   */
 }
