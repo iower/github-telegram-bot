ce770a768a368db6f7360dd8095ffa48e09f9ef3
iower <iower@tutanota.com>
2023-11-12 17:56:18 +0500

    Improve

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-console.log('Launch github-telegram-bot...')
+const IS_DEBUG = true;
 
-const fetch = require('node-fetch')
+console.log('Launch github-telegram-bot...');
+
+const fs = require('fs');
+const fetch = require('node-fetch');
 const TelegramBot = require('node-telegram-bot-api');
-const config = require('./config.json')
+const config = require('./config.json');
 
 const bot = new TelegramBot(config.TELEGRAM_BOT_TOKEN, {polling: true});
 
 bot.on('message', (msg) => {
-  console.log('message', msg)
+  console.log('message', msg);
   const chatId = msg.chat.id;
   if (chatId == config.TELEGRAM_OWNER_ID) {
     bot.sendMessage(chatId, 'Up!');
@@ -17,37 +20,72 @@ bot.on('message', (msg) => {
 // bot.sendMessage(config.TELEGRAM_OWNER_ID, 'Launched!');
 console.log('Launched!');
 
-const send = async () => {
-  const url = `https://api.github.com/repos/${config.GITHUB_REPO_OWNER_NAME}/${config.GITHUB_REPO_NAME}/commits`
-  const response = await fetch(url);
-  const commits = await response.json();
-  const commit = commits[1];
+const savedCommitsFileName = `saved-commits-${config.GITHUB_REPO_OWNER_NAME}-${config.GITHUB_REPO_NAME}`;
+const savedCommitsFilePath = `./${savedCommitsFileName}.json`;
 
+const process = async () => {
+  const url = `https://api.github.com/repos/${config.GITHUB_REPO_OWNER_NAME}/${config.GITHUB_REPO_NAME}/commits`;
+  const response = await fetch(url);
+  const fetchedCommits = await response.json();
+
+  // read
+  let savedCommits;
+  try {
+    savedCommits = require(savedCommitsFilePath);
+  } catch (e) {
+    console.info(`Will be created: ${savedCommitsFilePath}`);
+    saveCommits(fetchedCommits)
+    return;
+  }
+
+  const newCommits = [];
+  for (let i = 0; i < fetchedCommits; i++) {
+    const fetchedCommit = fetchedCommits[i];
+    if (!savedCommits.find(savedCommit => savedCommit.sha === fetchedCommit.sha)) {
+      newCommits.push(fetchedCommit);
+    }
+  };
+
+  if (newCommits.length) {
+    console.log(`New commits! (${newCommits.length})`);
+    saveCommits(fetchedCommits);
+    for (let i = 0; i < newCommits; i++) {
+      const newCommit = newCommits[i];
+      await sendCommitSummary(newCommit);
+    }
+  } else {
+    console.log('No new commits');
+  }
+}
+process();
+
+const saveCommits = (commits) => {
+  console.log(`Save commits to ${savedCommitsFilePath}`);
+  fs.writeFileSync(savedCommitsFilePath, JSON.stringify(commits), (e) => {
+    if (e) {
+      console.error(`Cannot write ${savedCommitsFilePath}`);
+      throw new Error(e)
+    }
+  });
+}
+
+const sendCommitSummary = (commit) => {
   const message = [
     `👤${commit?.author?.login || commit?.commit?.author?.name || comimt?.commit?.committer?.name || '?'}`,
     `📝${commit?.commit?.message || '?'}`,
     `🔗<a href='${commit?.html_url || '?'}'>${commit?.sha?.slice(0, 7) || '?'}</a>`
   ].join('\n')
 
-  console.log(commit)
-  console.log(message)
-
-  bot.sendMessage(config.TELEGRAM_OWNER_ID, message, {
-    parse_mode: 'HTML',
-    disable_web_page_preview: true
-  });
-
-  /*
-  bot.sendMessage(
-    config.TELEGRAM_CHAT_ID,
+  return bot.sendMessage(
+    IS_DEBUG ? config.TELEGRAM_OWNER_ID : config.TELEGRAM_CHAT_ID,
     message,
     {
-      reply_to_message_id: config.TELEGRAM_TOPIC_ID || undefined,
+      reply_to_message_id:
+        (!IS_DEBUG && Boolean(config.TELEGRAM_TOPIC_ID))
+          ? config.TELEGRAM_TOPIC_ID
+          : undefined,
       parse_mode : 'HTML',
-      disable_web_page_preview: true
+      disable_web_page_preview: true,
     }
   );
-  */
 }
-send();
-
